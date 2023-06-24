d493f32f77c3a19d50413899f46a5bb900c90c11
Igor Alekseev <ais_93@mail.ru>
2023-06-24 14:40:01 +0400

    App restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import React from 'react'
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className='flex flex-col gap-1'>
-      <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div className="p-6 bg-black border border-black" />
-        <div className="p-6 bg-black-subtle border border-black" />
-        <div className="p-6 bg-white border border-black" />
-        <div className="p-6 bg-blue-500 border border-black" />
-        <div className="p-6 bg-gray-50 border border-black" />
-        <div className="p-6 bg-gray-200 border border-black" />
-        <div className="p-6 bg-gray-300 border border-black" />
-        <div className="p-6 bg-gray-500 border border-black" />
-        <div className="p-6 bg-gray-700 border border-black" />
-      </div>
-      <div className="p-6 mx-auto bg-white rounded-xl shadow-lg text-left">
-        <div className="text-2xl font-semibold">Channel manager</div>
-        <div className="text-sm font-semibold text-black-subtle">Hotel</div>
-        <div className="text-base font-normal text-black-subtle">Hotel A</div>
-        <div className="text-sm font-semibold text-black-subtle">Channel</div>
-        <div className="text-sm font-semibold text-black-subtle">Visibility</div>
-        <div className="text-sm font-normal text-gray-700">Channel 1</div>
-      </div>
-    </div>
-  );
-}
+import { Main as MainPage } from './pages/Main';
 
-export default App;
+const App = () => (
+  <main>
+    <MainPage />
+  </main>
+);
+
+export { App };
