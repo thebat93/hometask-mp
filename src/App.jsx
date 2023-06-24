137513d0b8fd49dc10c36a8ef82d7d0f30819dce
Igor Alekseev <ais_93@mail.ru>
2023-06-24 13:20:57 +0400

    App initialization

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div className="shrink-0">
+        <img className="h-12 w-12" src={viteLogo} alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-slate-500">You have a new message!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
