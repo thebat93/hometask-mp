55320970705086ea77d30255c6114b0b9a86125d
Igor Alekseev <ais_93@mail.ru>
2023-06-24 13:40:24 +0400

    Color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-      <div className="shrink-0">
-        <img className="h-12 w-12" src={viteLogo} alt="ChitChat Logo" />
-      </div>
-      <div>
-        <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-slate-500">You have a new message!</p>
-      </div>
+    <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div className="p-6 bg-black border border-black" />
+      <div className="p-6 bg-black-subtle border border-black" />
+      <div className="p-6 bg-white border border-black" />
+      <div className="p-6 bg-blue-500 border border-black" />
+      <div className="p-6 bg-gray-50 border border-black" />
+      <div className="p-6 bg-gray-200 border border-black" />
+      <div className="p-6 bg-gray-300 border border-black" />
+      <div className="p-6 bg-gray-500 border border-black" />
+      <div className="p-6 bg-gray-700 border border-black" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      black: {
+        subtle: '#16181B',
+        DEFAULT: '#000000',
+      },
+      white: '#FFFFFF',
+      blue: {
+        500: '#0050FF',
+      },
+      gray: {
+        50: '#F8FAFC',
+        200: '#E2E8F0',
+        300: '#CBD5E1',
+        500: '#64748B',
+        700: '#334155',
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
